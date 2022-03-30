00e474034b5912f97d88a6c6623f06ac10aa0f52
Dave.L <dlau3335@gmail.com>
2022-03-30 16:31:41 -0400

    complete template

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,64 @@
 import './App.css';
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import { useDispatch, useSelector } from 'react-redux';
 // importing the function that modify redux state
-import { setData } from './store/reducer/stateReducer';
+import { setCategory, setCurrentItem } from './store/reducer/categoryReducer';
 
 function App() {
-  // const data = useSelector((state) => state);
-  // const dispatch = useDispatch();
+  const category = useSelector(state => state.category);
+  const dispatch = useDispatch();
 
+  useEffect(()=> {
+    const fetchData = async () => {
+      try {
+        //api call
+        const response = await axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products/?count=max',{
+          headers: {
+            'Authorization': process.env.REACT_APP_API_TOKEN
+          }
+        });
+        //set data to redux state
+        dispatch(setCategory(response.data));
+        if (!category.currentItem) {
+          dispatch(setCurrentItem(response.data[0]));
+        }
+      }
+      catch(err) {
+        console.log(err)
+      }
+    };
+    fetchData()
+  },[])
+console.log(category)
   return (
     <div className="App">
-      <p>my app</p>
+      <div style={{height:'5%', width:'100%', maxWidth: '1280px' , backgroundColor:'blue'}}>
+        header
+        {/* <Header/> */}
+      </div>
+      
+      <div style={{height:'60%', width:'100%', maxWidth: '1280px' , backgroundColor:'red'}}>
+        overview
+        {/* <OverView/> */}
+      </div>
+
+      <div style={{height:'40%', width:'100%', maxWidth: '1280px' , backgroundColor:'green'}}>
+      RelatedProducts && your outfit
+        {/* <RelatedProducts/> */}
+      </div>
+
+      <div style={{height:'35%', width:'100%', maxWidth: '1280px' , backgroundColor:'pink'}}>
+      q&a
+        {/* <QuestionAndAnswer/> */}
+      </div>
+
+      <div style={{height:'35%', width:'100%', maxWidth: '1280px' , backgroundColor:'purple'}}>
+      Rating And Reviews
+        {/* <RatingAndReviews/> */}
+      </div>
+      
+
     </div>
   );
 }
--- src/store/reducer/combineReducer.js
+++ src/store/reducer/combineReducer.js
@@ -1,6 +1,6 @@
 
-import {stateSlice} from './stateReducer'
-import {combineReducers} from 'redux'
+import {categorySlice} from './categoryReducer';
+import {combineReducers} from 'redux';
 
 
-export const allReducers = combineReducers({state: stateSlice.reducer})
+export const allReducers = combineReducers({category: categorySlice.reducer});
