5ef1d8ff4b19ae23c486b106f1f824ac6685b246
Lauren Anderson <laurendayle.anderson@gmail.com>
2022-04-05 15:53:23 -0400

    added sorting functions

--- src/utils/reviews/sortingUtils.js
+++ src/utils/reviews/sortingUtils.js
@@ -1,11 +1,43 @@
+import axios from 'axios';
+
 const sortByDate = (reviews) => {
   var sorted = reviews.sort((reviewOne, reviewTwo) => {
     return new Date(reviewOne.date) - new Date(reviewTwo.date);
   });
   return sorted.reverse();
-}
+};
 
+const sortByHelpfulness = (reviews) => {
+  var sorted = reviews.sort((reviewOne, reviewTwo) => {
+    return reviewOne.helpfulness - reviewTwo.helpfulness;
+  });
+  return sorted.reverse();
+};
+
+var sortByRelevance = async (id, reviews) => {
+  try {
+    const headers = {
+      'Authorization': process.env.REACT_APP_API_TOKEN
+    };
+    const relevant = await axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/reviews/?product_id=${id}&sort="relevant"&count=1000`, { headers });
+
+    return relevant;
+  }
+  catch(err) {
+    console.error(err);
+  }
+};
+
+// const sortByStarRating = (rating, reviews) => {
+//   return reviews.filter((review) => {
+//     if (review.rating === rating) {
+//       return review;
+//     }
+//   });
+// };
 
 module.exports = {
-  sortByDate: sortByDate
-}
+  sortByDate,
+  sortByRelevance,
+  sortByHelpfulness,
+};
