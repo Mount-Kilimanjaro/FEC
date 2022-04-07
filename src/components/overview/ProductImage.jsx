c453b88f626d8a6a7e198c1caf1a603cfe469cec
Dave.L <dlau3335@gmail.com>
2022-04-07 02:21:14 -0400

    added instruction to the statistic function

--- src/components/overview/ProductImage.jsx
+++ src/components/overview/ProductImage.jsx
@@ -10,12 +10,12 @@ export default function ProductImage(props) {
           <div id="imageWheel" className="p-2 hidden md:block absolute z-10">
             {imagesUrl.slice(0,5).map((photo,i) => 
             <div key={i} className={`p-0.5 `}>
-              <img id="scrollImg" name='overview_productImage_carousel_image' src={photo.url} alt="" className={`border-2 opacity-75 hover:opacity-100 hover:cursor-pointer hover:border-black ${imageUrl === imagesUrl[i].url? "border-b-blue-300 border-b-8 opacity-100" : ""}`} onClick={(e) => updateStatistic(changeImgUrl(photo.url), e.target.name)} />
+              <img id="scrollImg" name='overview_productImage_carousel_image' src={photo.url} alt="" className={`border-2 opacity-75 hover:opacity-100 hover:cursor-pointer hover:border-black ${imageUrl === imagesUrl[i].url? "border-b-blue-300 border-b-8 opacity-100" : ""}`} onClick={(e) => updateStatistic(changeImgUrl(photo.url), e)} />
             </div>
             )}
             {imagesUrl.length > 5 ?             
             <div className="flex mt-8 justify-center">
-              <img className="hover:cursor-pointer opacity-50 hover:opacity-100 select-none" name='overview_productImage_carousel_image_next' alt="" onClick={(e) => updateStatistic(carouselNextImage(),e.target.name)} src={arrowDown}/ >
+              <img className="hover:cursor-pointer opacity-50 hover:opacity-100 select-none" name='overview_productImage_carousel_image_next' alt="" onClick={(e) => updateStatistic(carouselNextImage(),e)} src={arrowDown}/ >
             </div>
             :
             <></>
@@ -23,8 +23,8 @@ export default function ProductImage(props) {
         </div>
         <div className="container flex flex-row justify-center items-center l w-full h-full">
           <div id="overview_arrow" className="flex md:hidden">
-            <img id="productImages_arrowLeft" className="overview_image_arrows hover:cursor-pointer opacity-50 hover:opacity-100 select-none mr-80" name='overview_productImage_main_arrow_left' alt="" onClick={(e) => updateStatistic(setPreviousImage(),e.target.name)} src={arrowLeft}/ >
-            <img id="productImages_arrowRight"className="overview_image_arrows hover:cursor-pointer opacity-50 hover:opacity-100 select-none" name='overview_productImage_main_arrow_right' alt="" onClick={(e) => updateStatistic(setNextImage(),e.target.name)} src={arrowRight}/ >
+            <img id="productImages_arrowLeft" className="overview_image_arrows hover:cursor-pointer opacity-50 hover:opacity-100 select-none mr-80" name='overview_productImage_main_arrow_left' alt="" onClick={(e) => updateStatistic(setPreviousImage(),e)} src={arrowLeft}/ >
+            <img id="productImages_arrowRight"className="overview_image_arrows hover:cursor-pointer opacity-50 hover:opacity-100 select-none" name='overview_productImage_main_arrow_right' alt="" onClick={(e) => updateStatistic(setNextImage(),e)} src={arrowRight}/ >
           </div>
           <img id="overview_main_img" className="h-full p-3 md:min-h-650" src={imageUrl} alt=""/>
         </div>
