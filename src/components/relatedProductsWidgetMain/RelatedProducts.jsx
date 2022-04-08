2fc53a975cdc0dad78fe1f8f71754aae878e7169
Cihad Akkaya <akkayac84@gmail.com>
2022-04-08 10:58:37 -0400

    fixed bug

--- src/components/relatedProductsWidgetMain/RelatedProducts.jsx
+++ src/components/relatedProductsWidgetMain/RelatedProducts.jsx
@@ -20,7 +20,7 @@ function RelatedProducts (props) {
   }
 
   return (
-    <div className='container'>
+    <div className='containerr'>
       <svg onClick={prevBut} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
   <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
 </svg>
