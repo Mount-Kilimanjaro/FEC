dbec1d93ffc6f562d5438fec6c5f2530c6a813e0
Cihad Akkaya <akkayac84@gmail.com>
2022-04-08 10:48:00 -0400

    finished relative items

--- src/components/relatedProductsWidgetMain/RelatedProducts.jsx
+++ src/components/relatedProductsWidgetMain/RelatedProducts.jsx
@@ -18,16 +18,25 @@ function RelatedProducts (props) {
     container.style.transitionDuration = '.8s';
     container.style.transform = `translate( -${currentCard * 245}px)`
   }
+
   return (
-    <div className='relatedProducts'>
-      <button id='prev' onClick={prevBut}>prev</button>
-      <div className='relatedProductsContent'>
+    <div className='container'>
+      <svg onClick={prevBut} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth={2}>
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15 19l-7-7 7-7" />
+</svg>
+<div className='relatedProducts'>
+
+<div className='relatedProductsContent'>
         {props.arr.map((value, index) => {
           return <Card prop1={value} key={index}/>
         })}
       </div>
-      <button id='next' onClick={nextBut}>next</button>
     </div>
+    <svg onClick={nextBut} xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M9 5l7 7-7 7" />
+</svg>
+    </div>
+
     )
 }
 
