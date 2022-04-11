420bbbf10d5424f431d6ab0ba80262612b542f3f
Cihad Akkaya <akkayac84@gmail.com>
2022-04-11 10:17:22 -0400

    implemented modals, implemented outfit

---
+++ src/components/relatedProductsWidgetMain/ComparisonModal.jsx
@@ -0,0 +1,42 @@
+import React, { useState, useEffect } from "react";
+import { useSelector } from "react-redux";
+
+
+
+function ComparisonM (props) {
+  const myItem = useSelector(state => state.category.currentItem);
+
+
+
+if (props.showModal) {
+  return (
+    <div className='ComparisonModal'>
+      <svg onClick={props.closeModal} xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 cancel_but" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M6 18L18 6M6 6l12 12" />
+</svg>
+<table>
+  <tr>
+    <th>Basic</th>
+    <th>Pro</th>
+    <th>comparison</th>
+  </tr>
+  <tr>
+    <td>Sample text</td>
+    <td>Sample text</td>
+    <td>Sample text</td>
+
+  </tr>
+  <tr>
+    <td>Sample text</td>
+
+  </tr>
+
+</table>
+    </div>
+  )
+} else {
+  return (<div></div>)
+}
+}
+
+export default ComparisonM;
--- src/components/relatedProductsWidgetMain/RelatedProductCard.jsx
+++ src/components/relatedProductsWidgetMain/RelatedProductCard.jsx
@@ -8,21 +8,33 @@ function RelatedProductCard (props) {
 
  if (props.prop1.photos[0].thumbnail_url) {
   return (
-   <div className='relatedProductCards'>
-      <img alt='shoe' src={props.prop1.photos[0].thumbnail_url}></img>
-      <h3>SHOES</h3>
-      <p>{props.prop1.name}</p>
-      <span>{props.prop1.original_price}</span>
-      <div className='stars'>review here</div>
+   <div name={props.prop1.style_id} className='relatedProductCards'>
+     <svg onClick={props.prop2} xmlns="http://www.w3.org/2000/svg" name={props.prop1.style_id} className="h-5 w-5 comparison_but" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" name={props.prop1.style_id} d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+</svg>
+<div onClick={props.addCard} name={props.prop1.style_id}>
+<img alt='shoe' src={props.prop1.photos[0].thumbnail_url} name={props.prop1.style_id}></img>
+      <h3 name={props.prop1.style_id}>SHOES</h3>
+      <p name={props.prop1.style_id}>{props.prop1.name}</p>
+      <span name={props.prop1.style_id}>{props.prop1.original_price}</span>
+      <div name={props.prop1.style_id} className='stars'>review here</div>
+</div>
+
     </div>
   )
  } else {
-    return<div className='relatedProductCards'>
-      <img alt='whatever' src={require('./assets/asset.jpg')}></img>
-      <h3>SHOES</h3>
-    <p>{props.prop1.name}</p>
-    <span>{props.prop1.original_price}</span>
-    <div className='stars'>review here</div>
+    return<div name={props.prop1.style_id} className='relatedProductCards'>
+      <svg onClick={props.prop2} xmlns="http://www.w3.org/2000/svg" name={props.prop1.style_id} className="h-5 w-5 comparison_but" viewBox="0 0 20 20" fill="currentColor">
+  <path fill-rule="evenodd" id={props.prop1.style_id} d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-8-3a1 1 0 00-.867.5 1 1 0 11-1.731-1A3 3 0 0113 8a3.001 3.001 0 01-2 2.83V11a1 1 0 11-2 0v-1a1 1 0 011-1 1 1 0 100-2zm0 8a1 1 0 100-2 1 1 0 000 2z" clip-rule="evenodd" />
+</svg>
+<div onClick={props.addCard} name={props.prop1.style_id}>
+<img name={props.prop1.style_id} alt='whatever' src={require('./assets/asset.jpg')}></img>
+      <h3 name={props.prop1.style_id}>SHOES</h3>
+    <p name={props.prop1.style_id}>{props.prop1.name}</p>
+    <span name={props.prop1.style_id}>{props.prop1.original_price}</span>
+    <div name={props.prop1.style_id} className='stars'>review here</div>
+</div>
+
     </div>
  }
 
