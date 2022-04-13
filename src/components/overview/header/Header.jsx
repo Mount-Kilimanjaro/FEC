213c5d9df127e143c694e870a96c426a909d1041
Dave.L <dlau3335@gmail.com>
2022-04-13 14:48:25 -0400

    updated with logo

--- src/components/overview/header/Header.jsx
+++ src/components/overview/header/Header.jsx
@@ -3,6 +3,7 @@ import ShoppingCart from "./ShoppingCart.jsx";
 import shoppingCartImg from "../assets/shopping_cart.svg";
 import Search from "../Search.jsx";
 import CheckoutModal from "./CheckoutModal.jsx";
+import logo from '../assets/logo.png'
 
 
 export default function Header(props) {
@@ -11,8 +12,12 @@ const [modalVisible, toggleModalVisible] = useState(false);
 const [checkoutModal, setCheckoutModal] = useState(false);
   return (
     <div id="header" className="container flex flex-row justify-between w-full h-60 bg-primary sticky top-0 z-[71]">
-        <div>
-            <p>logo</p>
+        <div className="flex">
+            <img className="h-[60px] ml-4" src={logo} alt="logo"/>
+            <div className="ml-1">
+              <p className="text-xl">Kilimanjaro</p>
+              <p className="ml-10 text-[#79592B]">STORE</p>
+            </div>
         </div>
         <Search cartModal={{handleToggleCart,cartVisibility}} modal={{modalVisible, toggleModalVisible}} toggleBlurBG={props.toggleBlurBG}/>
         <div className={`flex items-center justify-center w-3/6  md:w-150 md:rounded-l-2xl ${cartVisibility ? "bg-slate-300" : ""}  hover:cursor-pointer `} 
